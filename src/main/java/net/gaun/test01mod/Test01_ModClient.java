72e79f6815b995d7e174491ea0690f7c211ce260
seogau <sku379829@gmail.com>
2022-10-27 01:12:37 +0900

    Initial commit

---
+++ src/main/java/net/gaun/test01mod/Test01_ModClient.java
@@ -0,0 +1,10 @@
+package net.gaun.test01mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Test01_ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/gaun/test01mod/item/ModItems.java
@@ -0,0 +1,4 @@
+package net.gaun.test01mod.item;
+
+public class ModItems {
+}
