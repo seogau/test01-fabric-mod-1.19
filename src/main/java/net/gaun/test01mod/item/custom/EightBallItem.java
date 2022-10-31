7687f4bb6b4279262dfe00e376c8f917b41b85b2
seogau <sku379829@gmail.com>
2022-11-01 01:06:50 +0900

    #07 tooltips

--- src/main/java/net/gaun/test01mod/item/custom/EightBallItem.java
+++ src/main/java/net/gaun/test01mod/item/custom/EightBallItem.java
@@ -1,13 +1,20 @@
 package net.gaun.test01mod.item.custom;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.core.pattern.AbstractStyleNameConverter;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class EightBallItem extends Item {
     public EightBallItem(Settings settings) {
@@ -26,6 +33,18 @@ public class EightBallItem extends Item {
 
         return super.use(world, user, hand);
     }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()){
+            tooltip.add(Text.literal("Right Click to get a random number!").formatted(Formatting.AQUA));
+        }else{
+            tooltip.add(Text.literal("Press Shift for more info!").formatted(Formatting.YELLOW));
+        }
+
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+
     private void outputRandomNumber(PlayerEntity player){
         player.sendMessage(Text.literal("Your nubmer is "+ getRandomNumber()));
     }
