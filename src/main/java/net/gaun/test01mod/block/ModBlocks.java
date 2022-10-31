c6ff479be147855e7f98c280589553b4a5a1be81
seogau <sku379829@gmail.com>
2022-11-01 01:46:59 +0900

    #08 blockstates

--- src/main/java/net/gaun/test01mod/block/ModBlocks.java
+++ src/main/java/net/gaun/test01mod/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.gaun.test01mod.Test01_Mod;
 import net.gaun.test01mod.block.custom.JumpyBlock;
+import net.gaun.test01mod.block.custom.TanzaniteLampBlock;
 import net.gaun.test01mod.item.ModItemGroup;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
@@ -37,6 +38,11 @@ public class ModBlocks {
 
     public static final Block JUMPY_BLOCK = registerBlock("jumpy_block",
             new JumpyBlock(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()), ModItemGroup.TANZANITE);
+
+    public static final Block TANZANITE_LAMP = registerBlock("tanzanite_lamp",
+            new TanzaniteLampBlock(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()
+                    .luminance(state -> state.get(TanzaniteLampBlock.LIT) ? 15 : 0)), ModItemGroup.TANZANITE);
+
 
     private static Block registerBlock(String name, Block block, ItemGroup tab){
         registerBlockItem(name, block, tab);
